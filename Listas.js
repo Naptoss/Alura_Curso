c86538e1ed5b88aeefb64400f355afe3bcc65dbd
Naptoss <tomgabinio@gmail.com>
2022-12-23 22:35:57 -0300

    Detalhamento dos codigos

--- Listas.js
+++ Listas.js
@@ -3,5 +3,9 @@ const salvador = `Salvador`
 const saoPaulo = `São Paulo`
 const rioDeJaneiro = `Rio de Janeiro`
 
+console.log(`Destinos Possiveis: ${salvador}, ${saoPaulo}, ${rioDeJaneiro} `)
+// Uma forma desnecessaria de se fazer, muito codigo para uma função simples
 
-console.log(`DDestinos Possiveis: ${salvador}, ${saoPaulo}, ${rioDeJaneiro} `)
+const destinos = ["Paraiba ", "Recife ", "Natal ", "Rio de janeiro "]
+console.log(`Destinos possiveis: ${destinos}`)
+// Uma forma rapida e limpa de se realizar um codigo simples
