b7abe89a2e3d5fff1476d8141b408a4188ccb69d
Naptoss <tomgabinio@gmail.com>
2022-12-24 18:34:51 -0300

    .

--- Listas.js
+++ Listas.js
@@ -1,14 +1,14 @@
-console.log(`Trabalhando com listas`)
-const salvador = `Salvador`
-const saoPaulo = `São Paulo`
-const rioDeJaneiro = `Rio de Janeiro`
+// console.log(`Trabalhando com listas`)
+// const salvador = `Salvador`
+// const saoPaulo = `São Paulo`
+// const rioDeJaneiro = `Rio de Janeiro`
 
-console.log(`Destinos Possiveis: ${salvador}, ${saoPaulo}, ${rioDeJaneiro} `)
-// Uma forma desnecessaria de se fazer, muito codigo para uma função simples
+// console.log(`Destinos Possiveis: ${salvador}, ${saoPaulo}, ${rioDeJaneiro} `)
+    // Uma forma desnecessaria de se fazer, muito codigo para uma função simples
 
-const destinos = ["Paraiba ", "Recife ", "Natal ", "Rio de janeiro "]
-console.log(`Destinos possiveis: ${destinos}`)
-// Uma forma rapida e limpa de se realizar um codigo simples
+// const destinos = ["Paraiba ", "Recife ", "Natal ", "Rio de janeiro "]
+// console.log(`Destinos possiveis: ${destinos}`)
+    // Uma forma rapida e limpa de se realizar um codigo simples
 
 const listaDeDestinos = new Array(
             `Salvador`,
@@ -18,4 +18,8 @@ const listaDeDestinos = new Array(
 listaDeDestinos.push("Recife")
     // Usando a função push para adicionnar mais uma cidade na lista
 
-console.log("Lista de destinos:",listaDeDestinos)
+listaDeDestinos.splice(2)
+    // a função splice serve para remover um elemento da lista 
+
+
+console.log("Lista de destinos:",listaDeDestinos[1]) // nesse console.log serve para imprimir um valor unico 
