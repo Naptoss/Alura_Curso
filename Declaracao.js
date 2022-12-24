3fe987d8bf2f6dc18aca7ddd17808f264d1cba0f
Naptoss <tomgabinio@gmail.com>
2022-12-23 22:27:55 -0300

    Formas de declaração em js

---
+++ Declaracao.js
@@ -0,0 +1,18 @@
+const primeiroNome= "Antonio" 
+const sobrenome = "Gabinio"
+let idade // Declarando uma variavel
+idade = 26 //registrando um valor a essa variavel
+
+console.log(primeiroNome, sobrenome)
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+let contador = 0
+contador = contador +1
+
+console.log(contador)
+
+
+const nomeCompleto = primeiroNome + sobrenome
+console.log(nomeCompleto) 
+
