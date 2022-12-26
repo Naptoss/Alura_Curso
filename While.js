48ff0ea9b1f2472469adb348d475bb6916315f02
Naptoss <tomgabinio@gmail.com>
2022-12-26 11:08:38 -0300

    adicionado o break

--- While.js
+++ While.js
@@ -8,7 +8,7 @@ const listaDeDestinos = new Array(
 const idadeComprador = 18
 const estaAcompanhada = false
 let temPassagemComprada = false
-const destino = "Curitiba"
+const destino = "Rio de Janeiro"
 
 console.log("\n Destinos possiveis:")
 console.log(listaDeDestinos)
@@ -16,12 +16,17 @@ console.log(listaDeDestinos)
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true
 
 let contador = 0
+let destinoExiste= false
 while(contador<3){
+
     if(listaDeDestinos[contador] == destino){
         console.log("Destino existe")
-    } else {
-        console.log("Destino não existe")
+        destinoExiste = true
+        break
+
     }
     contador += 1
 
 }
+
+console.log("Destino existe:", destinoExiste)
